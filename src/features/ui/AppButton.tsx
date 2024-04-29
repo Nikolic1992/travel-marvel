9ceff6b9611ac553706cb12e20f152778e73ee24
Stevan Nikolic <133847825+Nikolic1992@users.noreply.github.com>
2024-04-29 16:01:46 +0200

    chore: firebase hosting and GA setup (#4)

--- src/features/ui/AppButton.tsx
+++ src/features/ui/AppButton.tsx
@@ -1,29 +1,41 @@
-import { LoadingButton } from '@mui/lab';
+import LoadingButton from '@mui/lab/LoadingButton';
 import { type SxProps, type Theme, Typography } from '@mui/material';
 
 interface Props {
-  type: 'button' | 'submit' | 'reset';
+  type?: 'button' | 'submit' | 'reset';
   variant?: 'text' | 'contained' | 'outlined';
   fullWidth?: boolean;
   loading?: boolean;
+  endIcon?: React.ReactNode;
   children: React.ReactNode;
+  onClick?: () => void;
+  LinkComponent?: React.ElementType;
+  href?: string;
   sx?: SxProps<Theme>;
 }
 
 export default function AppButton({
-  type,
+  type = 'button',
   variant = 'contained',
   fullWidth,
   loading,
+  LinkComponent,
+  href,
   children,
+  endIcon,
   sx,
+  onClick,
 }: Props) {
   return (
     <LoadingButton
       loading={loading}
+      LinkComponent={LinkComponent}
+      href={href}
       fullWidth={fullWidth}
       type={type}
       variant={variant}
+      endIcon={endIcon}
+      onClick={onClick}
       sx={{
         borderRadius: 2,
         height: { xs: 48, md: 56 },
