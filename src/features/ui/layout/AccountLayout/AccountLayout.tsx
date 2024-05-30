2370a031bc94a85af7ed8ac894ac3aeaad73bd44
Stevan Nikolic <133847825+Nikolic1992@users.noreply.github.com>
2024-05-30 18:35:51 +0200

    fix: account overflow and sidebar button (#18)

--- src/features/ui/layout/AccountLayout/AccountLayout.tsx
+++ src/features/ui/layout/AccountLayout/AccountLayout.tsx
@@ -79,9 +79,7 @@ export default function AccountLayout() {
       sx={{
         display: 'flex',
         bgcolor: 'grey.100',
-        minHeight: { md: '100vh' },
-        height: { xs: '100vh', md: 'auto' },
-        maxHeight: { xs: '-webkit-fill-available', md: 'auto' },
+        minHeight: '100vh',
       }}
     >
       {/* Desktop Drawer */}
@@ -96,7 +94,7 @@ export default function AccountLayout() {
             onClick={handleDrawerToggle}
             sx={{
               borderRadius: 1,
-              position: 'absolute',
+              position: 'fixed',
               top: 27,
               left: `calc(${
                 isOpen ? DESKTOP_DRAWER_WIDTH : DESKTOP_MINIMIZED_DRAWER_WIDTH
