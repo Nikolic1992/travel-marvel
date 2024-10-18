d32776e3913bf0ff50e549394ccd0d844e0fe918
Nikolic1992 <stevannikolic1992@yahoo.com>
2024-10-18 20:38:31 +0200

    feat: save selected tab in the URL

--- src/features/trip/edit/Tabs/TripTabs.tsx
+++ src/features/trip/edit/Tabs/TripTabs.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 import CameraEnhanceIcon from '@mui/icons-material/CameraEnhance';
 import ChecklistIcon from '@mui/icons-material/Checklist';
@@ -9,6 +10,7 @@ import { Box, Tab, Tabs } from '@mui/material';
 
 import type { Trip } from '../../types';
 import Documents from './Documents';
+import PackingLists from './PackingLists';
 import Photos from './Photos';
 import TripInfoAndPlaces from './TripInfoAndPlaces';
 
@@ -37,11 +39,20 @@ function CustomTabPanel({
     </div>
   );
 }
+
 export default function TripTabs({ trip, onUpdate }: Props) {
-  const [selectedTab, setSelectedTab] = useState(0);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [selectedTab, setSelectedTab] = useState(
+    Number(searchParams.get('selectedTab')) ?? 0,
+  );
+
   const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
     setSelectedTab(newValue);
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set('selectedTab', newValue.toString());
+    setSearchParams(newSearchParams);
   };
+
   return (
     <Box sx={{ width: '100%' }}>
       <Tabs
@@ -93,7 +104,7 @@ export default function TripTabs({ trip, onUpdate }: Props) {
         <Documents trip={trip} onUpdate={onUpdate} />
       </CustomTabPanel>
       <CustomTabPanel value={selectedTab} index={2}>
-        Packing List
+        <PackingLists trip={trip} onUpdate={onUpdate} />
       </CustomTabPanel>
       <CustomTabPanel value={selectedTab} index={3}>
         Expenses
