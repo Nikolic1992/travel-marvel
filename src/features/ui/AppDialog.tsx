ec3bb15a7d7875bc4e65f18d0d76da160f91dd81
Nikolic1992 <stevannikolic1992@yahoo.com>
2025-01-08 20:04:27 +0100

    fix: fixed Yes button

--- src/features/ui/AppDialog.tsx
+++ src/features/ui/AppDialog.tsx
@@ -129,7 +129,6 @@ export default function AppDialog({
               onClick={onPrimaryButtonClick}
               loading={isLoading}
             >
-              {secondaryButtonText}
               {primaryButtonText}
             </AppButton>
           )}
