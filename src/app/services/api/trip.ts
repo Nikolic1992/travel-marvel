f2a53a3b577fd6f1d44b5100e34b874586ea3437
Nikolic1992 <stevannikolic1992@yahoo.com>
2025-01-22 19:45:35 +0100

    feat: add trip count limit and order query

--- src/app/services/api/trip.ts
+++ src/app/services/api/trip.ts
@@ -4,6 +4,8 @@ import {
   doc,
   getDoc,
   getDocs,
+  limit,
+  orderBy,
   query,
   setDoc,
   updateDoc,
@@ -17,34 +19,42 @@ function authenticate<T>(authenticatedFn: () => Promise<T>) {
   if (!auth.currentUser) {
     throw Error('Looks like you are not-authorized to make this change!');
   }
-
   return authenticatedFn();
 }
 
-export async function getTrips() {
+export async function getTrips(tripsLimit?: number) {
   return authenticate(async () => {
-    const userTripsQuery = query(
-      collection(firestore, 'trips'),
-      where('userUid', '==', auth.currentUser!.uid),
-    );
+    let userTripsQuery;
+
+    if (tripsLimit) {
+      userTripsQuery = query(
+        collection(firestore, 'trips'),
+        where('userUid', '==', auth.currentUser!.uid),
+        orderBy('startDate', 'desc'),
+        limit(tripsLimit),
+      );
+    } else {
+      userTripsQuery = query(
+        collection(firestore, 'trips'),
+        where('userUid', '==', auth.currentUser!.uid),
+        orderBy('startDate', 'desc'),
+      );
+    }
 
     const querySnapshot = await getDocs(userTripsQuery);
 
-    const fetchedtrips = querySnapshot.docs.map((doc) => doc.data() as Trip);
-
-    const tripsPormises = await Promise.allSettled(
-      fetchedtrips.map(async (trip) => {
+    const fetchedTrips = querySnapshot.docs.map((doc) => doc.data() as Trip);
+    const tripsPromises = await Promise.allSettled(
+      fetchedTrips.map(async (trip) => {
         if (trip.previewImage?.storagePath) {
           trip.previewImage.url = await getDownloadURL(
             trip.previewImage?.storagePath,
           );
         }
-
         return trip;
       }),
     );
-
-    const tripsWithPreviewImages = tripsPormises
+    const tripsWithPreviewImages = tripsPromises
       .map((trip) => {
         if (trip.status === 'fulfilled') {
           return trip.value;
@@ -53,36 +63,28 @@ export async function getTrips() {
         }
       })
       .filter(Boolean);
-
     return tripsWithPreviewImages as Trip[];
   });
 }
-
 export async function getTripById(tripId?: string) {
   return authenticate(async () => {
     if (!tripId) {
       throw new Error('Trip not found!');
     }
-
     const tripRef = doc(firestore, 'trips', tripId);
     const tripSnap = await getDoc(tripRef);
-
     if (!tripSnap.exists()) {
       throw new Error('Trip not found!');
     }
-
     const trip = tripSnap.data() as Trip;
-
     if (trip.previewImage?.storagePath) {
       trip.previewImage.url = await getDownloadURL(
         trip.previewImage?.storagePath,
       );
     }
-
     return trip;
   });
 }
-
 export async function addTrip(trip: Trip) {
   return authenticate(async () => {
     await setDoc(doc(firestore, 'trips', trip.id), {
@@ -91,21 +93,17 @@ export async function addTrip(trip: Trip) {
     });
   });
 }
-
 export async function updateTrip(tripId: string, data: Partial<Trip>) {
   return authenticate(async () => {
     const tripRef = doc(firestore, 'trips', tripId);
     await updateDoc(tripRef, data);
-
     return true;
   });
 }
-
 export async function deleteTrip(tripId: string) {
   return authenticate(async () => {
     const tripRef = doc(firestore, 'trips', tripId);
     await deleteDoc(tripRef);
-
     return true;
   });
 }
