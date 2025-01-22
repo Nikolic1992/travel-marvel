84a465217cdd82452768226bd360e38f3d22f213
Nikolic1992 <stevannikolic1992@yahoo.com>
2025-01-22 18:50:24 +0100

    feat: add extra dates validation to trip forms

--- src/features/trip/add-trip/components/Steps/TripInfo.tsx
+++ src/features/trip/add-trip/components/Steps/TripInfo.tsx
@@ -137,6 +137,15 @@ export default function TripInfo() {
               requireErrorText="Please specify start date!"
               maxDate={formValues.endDate}
               fullWidth
+              validate={{
+                startDate: (startDate) =>
+                  !startDate ||
+                  (startDate &&
+                    formValues.endDate &&
+                    startDate < formValues.endDate)
+                    ? undefined
+                    : 'Start date should be before end date!',
+              }}
             />
             <DateSelectInput
               label="End date"
@@ -145,6 +154,15 @@ export default function TripInfo() {
               requireErrorText="Please specify end date!"
               minDate={formValues.startDate}
               fullWidth
+              validate={{
+                endDate: (endDate) =>
+                  !endDate ||
+                  (endDate &&
+                    formValues.startDate &&
+                    formValues.startDate < endDate)
+                    ? undefined
+                    : 'End date should be after start date!',
+              }}
             />
           </Stack>
         </Stack>
